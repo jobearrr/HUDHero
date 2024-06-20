bc2204117e52dddd43e73c48a5464400ae6d0c3a
Jobert Sá <bearinmind@jobear.dev>
2024-06-20 19:48:23 +0200

    📦 Updated macOS package spec

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "HUDHero",
     platforms: [
       .iOS(.v15),
-      .macOS(.v10_15),
+      .macOS(.v12),
       .tvOS(.v13),
       .watchOS(.v6),
       .visionOS(.v1)
