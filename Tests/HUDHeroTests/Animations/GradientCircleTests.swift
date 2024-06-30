44eb79050dbc82c1df7b959f0b2a5eaa1bd13f40
Jobert Sá <bearinmind@jobear.dev>
2024-06-30 22:46:33 +0200

    🧪 GradientCircle animation tests

---
+++ Tests/HUDHeroTests/Animations/GradientCircleTests.swift
@@ -0,0 +1,22 @@
+//
+//  GradientCircleTests.swift
+//  
+//
+//  Created by Jobert Sá on 30/06/2024.
+//
+
+import XCTest
+import SwiftUI
+@testable import HUDHero
+
+final class GradientCircleTests: XCTestCase {
+
+    func testGradientCircleDefault() {
+        let gradientCircle = GradientCircle.default
+        
+        XCTAssertEqual(gradientCircle.config.iconName, "circle")
+        XCTAssertEqual(gradientCircle.config.foregroundColor, .primary)
+        XCTAssertEqual(gradientCircle.config.size, 30)
+        XCTAssertEqual(gradientCircle.config.rps, 1)
+    }
+}
